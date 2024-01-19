ad1a2c71993534dad92aa0910836c5cacd149a73
alexgm5555 <alexgm5555@hotmail.com>
2024-01-19 14:38:58 -0500

    feat: rest components

---
+++ src/app/components/SelectedProduct/page.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { useEffect, useState } from 'react';
+import styles from './page.module.scss'
+import { ProductsInterface } from '@/app/lib/interfaces';
+import { sumTotal } from '../../../../provider/redux/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import Products from '../Products/page';
+
+const SelectedProduct:any = () =>{
+  const [products, setProducts] = useState<ProductsInterface[]>();
+
+  const data = useSelector((state: any) => state.user.products);
+  const dispatch =  useDispatch();
+
+  const sortPerQuatity = (arrayOld: ProductsInterface[]) => {
+    const arrayNew: ProductsInterface[] = [];
+    arrayOld.forEach((e: ProductsInterface)=> {
+      const a = data.filter((a: ProductsInterface)=>a.id === e.id);
+      if(!arrayNew.find((b: ProductsInterface)=>b.id === e.id))
+        arrayNew.push({ ...e, quantity: a.length})
+    });
+    return arrayNew;
+  }
+
+  useEffect(() => {
+    setProducts(sortPerQuatity(data));
+    
+    let result = 0;
+    for (let i in data) {
+      result += parseFloat(data[i].cost) ;
+    }
+    dispatch(sumTotal({
+      total: result
+    }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  return (
+    <div className={`${styles['SeletedProducts-container']}`}>
+      <Products
+        products={products}
+        title='Remove product'
+        action='remove'
+      />
+    </div>
+)}
+
+export default SelectedProduct;
---
+++ src/app/components/ShoppingCar/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { useSelector } from 'react-redux';
+import styles from './page.module.css'
+import Image from 'next/image';
+import { Suspense } from 'react';
+// import ButtonWompi from '../ButtonWompi/page';
+
+const ShoppingCar:any = () =>{
+  const data = useSelector((state: any) => state.user);
+console.log(data);
+
+  return (
+    <div 
+      className={`${styles['ShoppingCar-container']}`}>
+      <div className={`${styles['div-car']}`}>
+        <span className={`${styles['dot']}`}>
+          <span className={`${styles['_span']}`}>
+            {data.products.length | 0}
+          </span>
+        </span>
+        <Image 
+          className={`${styles['img-car']}`}
+          src='/images/shoppin_car.png'
+          alt=""
+          width={100}
+          height={100}
+        />
+      </div>
+      <div className={`${styles['total-span']}`}>
+        {/* {data.total !== 0 && 
+        <Suspense fallback={<>cargando...</>}>
+          <ButtonWompi/>
+        </Suspense>
+        } */}
+      </div>
+    </div>
+)}
+export default ShoppingCar;
